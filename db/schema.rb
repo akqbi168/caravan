1611ad84852e18d1ef7b59dd962fe64912969a4f
akqbi168 <akqbi168@gmail.com>
2019-01-10 16:53:19 +0900

    [Finish] 4-2

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_10_070727) do
+ActiveRecord::Schema.define(version: 2019_01_10_071734) do
 
   create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.string "string"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
