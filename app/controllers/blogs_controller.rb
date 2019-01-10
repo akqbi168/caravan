8936a8d7460c28323d8d05a676d3357646b17c21
akqbi168 <akqbi168@gmail.com>
2019-01-10 17:19:24 +0900

    [Finish] 4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,26 @@
 class BlogsController < ApplicationController
-  def show
-  end
+	def show
+	end
 
-  def index
-  end
+	def index
+	end
 
-  def new
-  end
+	def new
+		@blog = Blog.new
+	end
 
-  def edit
-  end
+	def create
+		blog = Blog.new(blog_params)
+		blog.save # DB保存
+		redirect_to blogs_path
+	end
+
+	def edit
+	end
+
+	private
+
+	def blog_params
+		params.require(:blog).permit(:title, :category, :body)
+	end
 end
